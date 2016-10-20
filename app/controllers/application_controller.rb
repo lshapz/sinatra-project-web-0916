10a65390735f4e61875e9ff747059c2655334bd1
lshapz <laurashapiro01@gmail.com>
2016-10-19 20:20:31 -0400

    blocked entire website with a password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set(:views, 'app/views')
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == 'admin' and password == 'admin'
+end
+
 get '/' do 
   erb :'index', :layout => false
 end
