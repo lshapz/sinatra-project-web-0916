f63267309cac7feff40edae69690b06a8a4c87cf
lshapz <laurashapiro01@gmail.com>
2016-10-20 13:39:59 -0400

    users have .categories methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :user_favorites 
 # has_many :restaurants, through: :user_restaurants
   has_many :restaurants, through: :user_favorites
-
+  has_many :categories, through: :restaurants
 end 
